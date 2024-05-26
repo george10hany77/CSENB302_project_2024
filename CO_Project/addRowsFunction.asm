dc6ac84b2f1a04f9826a58875df468beb53f52eb
George Hany <georgehany064@gmail.com>
2024-05-26 23:25:35 +0200

    adding more features to the macros to be generic

--- CO_Project/addRowsFunction.asm
+++ CO_Project/addRowsFunction.asm
@@ -14,23 +14,33 @@
     colLen db 4
     
 .code 
-
+ 
+ 
 ; Helper macro to give it i, j and it transforms it into single coordinate.
 
-CONVERT MACRO i,j,ans 
-    mov al, colLen
+CONVERT MACRO i, j, ans, localColumnLength 
+    mov al, localColumnLength
     mov ah, i
     mul ah
     add al, j 
     mov ans, al  
 endm
-
-FILLARR macro arr,val,len
+ 
+  
+; Helper macro to fill the array with specific values
+  
+FILLARR macro arr, val, localRowLength, localColumnLength
     local initL ; declare initL lable as local
     
-    ; init array
+    mov ax, 0
+    mov bx, 0
     
-    mov cx, len
+    ; init array
+    mov al, localRowLength
+    mov bl, localColumnLength 
+    mul bl ; to store the length of the whole 1D array into the AX
+    
+    mov cx, ax
     dec cx  ; to match the index of the array 
     mov di, offset arr ; to get the ofset address in memory
     
@@ -68,7 +78,7 @@ ADDROUNDKEY macro sArr, rkArr, localRowLength, localColumnLength
             cmp bh, localRowLength
             jz outerL
             
-            CONVERT row, col, index
+            CONVERT row, col, index, rowLen, colLen
             ; beginning xor operation
             mov bl, index                                          
             mov bh, 0                                              
@@ -95,78 +105,80 @@ main PROC
     ; My Code..........>>>>>
                 
     
-    FILLARR state,99h,16
-    FILLARR roundKey, 87h, 16
+    FILLARR state,99h, rowLen, colLen
+    FILLARR roundKey, 87h, rowLen, colLen
                 
-    CONVERT 0,0,index
+    CONVERT 0,0,index, colLen
     mov bl, index
-    mov state[bx], 29h            
+    mov state[bx], 29h    
     
-    CONVERT 1,0,index
+    CONVERT 1,0,index, colLen
     mov bl, index
-    mov state[bx], 29h            
+    mov state[bx], 29h    
     
-    CONVERT 2,0,index
+    CONVERT 2,0,index, colLen
     mov bl, index
-    mov state[bx], 29h            
+    mov state[bx], 29h    
     
-    CONVERT 3,0,index
+    CONVERT 3,0,index, colLen
     mov bl, index
     mov state[bx], 29h
     
-    CONVERT 0,1,index
+    CONVERT 0,1,index, colLen
     mov bl, index
-    mov state[bx], 34h            
+    mov state[bx], 34h    
     
-    CONVERT 1,1,index
+    CONVERT 1,1,index, colLen
     mov bl, index
-    mov state[bx], 34h            
+    mov state[bx], 34h    
     
-    CONVERT 2,1,index
+    CONVERT 2,1,index, colLen
     mov bl, index
-    mov state[bx], 34h            
+    mov state[bx], 34h    
     
-    CONVERT 3,1,index
+    CONVERT 3,1,index, colLen
     mov bl, index
-    mov state[bx], 34h            
+    mov state[bx], 34h    
                 
-    CONVERT 0,2,index
+    CONVERT 0,2,index, colLen
     mov bl, index
-    mov state[bx], 43h            
+    mov state[bx], 43h    
     
-    CONVERT 1,2,index
+    CONVERT 1,2,index, colLen
     mov bl, index
-    mov state[bx], 43h            
+    mov state[bx], 43h    
     
-    CONVERT 2,2,index
+    CONVERT 2,2,index, colLen
     mov bl, index
-    mov state[bx], 43h            
+    mov state[bx], 43h    
     
-    CONVERT 3,2,index
+    CONVERT 3,2,index, colLen
     mov bl, index
     mov state[bx], 43h
     
-    CONVERT 0,3,index
+    CONVERT 0,3,index, colLen
     mov bl, index
-    mov state[bx], 76h            
+    mov state[bx], 76h    
     
-    CONVERT 1,3,index
+    CONVERT 1,3,index, colLen
     mov bl, index
-    mov state[bx], 76h            
+    mov state[bx], 76h    
     
-    CONVERT 2,3,index
+    CONVERT 2,3,index, colLen
     mov bl, index
-    mov state[bx], 76h            
+    mov state[bx], 76h    
     
-    CONVERT 3,3,index
+    CONVERT 3,3,index, colLen
     mov bl, index
     mov state[bx], 76h
     
     
+    ;The actual function of addRoundKey
+    
     ADDROUNDKEY state, roundKey, rowLen, colLen                          
-              
-    mov bx, 0
-    mov ax, 0 
+    
+    ; end of the macro          
+  
                       
     ; Exit program
     
@@ -197,85 +209,6 @@ END main          ; End of program
 
 
 
-
-
-
-
-
-
-
-
-
-
-
- 
-; .model small      ; Define memory model as 'small'
-;.stack 1000h       ; Define stack size
-;
-;.data 
-;
-;    index db 0
-;    state db 4 dup(4 dup (?))    
-;    roundKey db 4 dup(4 dup (?)) 
-;    arrLen dw 16   
-;
-;.code 
-;
-;; Helper macro to give it i, j and it transforms it into single coordinate.
-;
-;CONVERT MACRO i, j, ans 
-;    mov al, 4
-;    mov ah, i
-;    mul ah
-;    add al, j 
-;    mov ans, al  
-;endm
-;
-;; Corrected FILLARR macro
-;FILLARR macro arr, val, len
-;    mov cx, len      ; Load length into CX
-;    mov si, OFFSET arr ; Load offset of the array into SI
-;    initL:
-;        mov BYTE PTR [si], val  ; Move the value into the array at [SI]
-;        inc si                  ; Increment SI to point to the next element
-;        loop initL              ; Loop until CX is 0
-;endm    
-;
-;; Placeholder for ADDROUNDKEY macro
-;ADDROUNDKEY macro sArr, rkArr
-;    ; Macro body here
-;endm
-;
-;main PROC
-;    ; Set up data segment
-;    mov ax, @data     ; Load data segment address into AX
-;    mov ds, ax        ; Move data segment address from AX to DS
-;    mov es, ax        ; Move data segment address from AX to ES (if needed)  
-;    
-;    ; My Code
-;    CONVERT 3, 3, index
-;    FILLARR state, 98h, arrLen                 
-;    
-;    ; Exit program
-;    mov ax, 4C00h     ; DOS interrupt for program termination
-;    int 21h           ; Call DOS interrupt
-;main ENDP           ; End of main procedure 
-;
-;END main            ; End of program
-;
-       
-       
-       
-       
-       
-       
-       
-       
-       
-       
-       
-       
-     
      
      
     
